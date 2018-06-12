a4bb2671a6bce10d65d6c1667de8bdf0c0a0717e
MoLeighJackson <moleighjackson@gmail.com>
2018-06-11 21:34:30 -0400

    Added final score

--- app/src/main/java/com/example/moleigh/gettoknowgraphene/MainActivity.java
+++ app/src/main/java/com/example/moleigh/gettoknowgraphene/MainActivity.java
@@ -4,12 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     int score = 0;
+    String answer = "scotch";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,8 +64,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    public void addPointEditText(View view) {
+
+        EditText editTextAnswer = (EditText) findViewById(R.id.edit_text_answer);
+        answer = editTextAnswer.getText().toString();
+
+        if(answer.equals("scotch")) {
+            displayScore(score + 1);
+            finalScore(score + 1);
+            Toast.makeText(this, "Correct", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Incorrect. Try Again.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
     public void showToast(View view) {
         Toast.makeText(this, "Incorrect. Try Again.", Toast.LENGTH_SHORT).show();
+
+        CheckBox checkbox2 = (CheckBox)findViewById(R.id.checkbox_answer2);
+        CheckBox checkbox4 = (CheckBox)findViewById(R.id.checkbox_answer4);
+
+        if (checkbox2.isChecked()) {
+            checkbox2.toggle();
+        }
+
+        if (checkbox4.isChecked()) {
+            checkbox4.toggle();
+        }
     }
 
     /**
@@ -70,6 +102,11 @@ public class MainActivity extends AppCompatActivity {
      */
     public void displayScore(int score) {
         TextView scoreView = (TextView) findViewById(R.id.player_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void finalScore(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.final_score);
         scoreView.setText(String.valueOf(score));
     }
 
